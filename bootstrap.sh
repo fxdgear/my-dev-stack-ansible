e9a020e26acd90f03f31643ab5afcbb4bb08e805
Nick Lang <nick@nicklang.com>
2023-10-27 20:41:10 -0600

    update vars

--- bootstrap.sh
+++ bootstrap.sh
@@ -35,7 +35,7 @@ fi
 
 # Run the Ansible playbook
 echo "Running the Ansible playbook..."
-ansible-playbook -i localhost, "$playbook_file" --ask-become-pass -v -e "user_home=$HOME" -e "user=$USER"
+ansible-playbook -i localhost, "$playbook_file" --ask-become-pass -v -e "user_home=$HOME" -e "user_name=$USER"
 # Clean up (optional)
 cd ..
 rm -rf "$ansible_project_name"
